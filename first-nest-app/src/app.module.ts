0393b8e73405dbdbd839326aadb1a02e5f3c277a
Theo AVRIL <t.avril@epack-hygiene.fr>
2024-08-08 16:59:10 +0200

    Implémentation de la partie Services et injection de dépendances.
     Changes to be committed:
    	modified:   first-nest-app/src/app.module.ts
    	new file:   first-nest-app/src/dto/sayname.dto.ts
    	new file:   first-nest-app/src/sayname/sayname.controller.spec.ts
    	new file:   first-nest-app/src/sayname/sayname.controller.ts
    	new file:   first-nest-app/src/sayname/sayname.module.ts
    	new file:   first-nest-app/src/sayname/sayname.service.spec.ts
    	new file:   first-nest-app/src/sayname/sayname.service.ts

--- first-nest-app/src/app.module.ts
+++ first-nest-app/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SumServiceService } from './sum-service/sum-service.service';
+import { SaynameModule } from './sayname/sayname.module';
 
 @Module({
-  imports: [],
+  imports: [SaynameModule],
   controllers: [AppController],
   providers: [AppService, SumServiceService],
 })
